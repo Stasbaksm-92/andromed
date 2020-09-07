73d2219a8d0b18b89e0b925e639fc290fabdeca3
stas <stas.baksm@gmail.com>
2020-09-07 18:06:01 +0300

    validete

--- src/js/script.js
+++ src/js/script.js
@@ -46,5 +46,44 @@ $(document).ready(function () {
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
+    /*  $('#consultation-form').validate();
+            $('#consultation form').validate({
+            /  rules: {
+            name: "required",
+            phone: {
+                required: true,
+                number: true
+            }
+        },
+        messages: {
+            name: "Пожалуйста, введите Ваше имя",
+            phone: {
+                required: "Пожалуйста,введите Ваш мобильный номер",
+                phone: "Введите в формате +380..."
+            }
+        }
+    }); 
+ */
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: {
+                    required: true,
+                    number: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите Ваше имя",
+                phone: {
+                    required: "Пожалуйста,введите Ваш мобильный номер",
+                    phone: "Введите в формате +380..."
+                }
+            }
+        });
 
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
 });
